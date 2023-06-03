5201f808a644b766f66903f2deecf3fdf9105c9c
Daniel Skýpala <daniel@honza.info>
2023-06-03 13:03:07 +0200

    Building AST

--- lcalc.hs
+++ lcalc.hs
@@ -1,11 +1,23 @@
 import Data.Char
+import Debug.Trace
 
-data LNamed = LName String
+debug = flip trace
+-- Utilities
+contains :: Eq a => a -> [a] -> Bool
+contains x [] = False
+contains x (y:rs) 
+    | x == y    = True
+    | otherwise = contains x rs
+
+-- Tokenization
 data LToken
     = LNamed String |
       LLambda | LDot |
       LOpen | LClose
     deriving Show
+
+isSpecial x = contains x "()\\. "
+isNotSpecial = not . isSpecial
 
 tokenize :: String -> [LToken]
 tokenize [] = []
@@ -16,7 +28,34 @@ tokenize ('.':rs) = LDot : tokenize rs
 tokenize (' ':rs) = tokenize rs
 tokenize r@(r0:rs)
     | nextToken == "" = error ("Unknown symbol " ++ [r0])
-    | otherwise = (LNamed nextToken):(tokenize $ dropWhile isSpace r)
+    | otherwise = (LNamed nextToken):(tokenize $ dropWhile isNotSpecial r)
     where
-        nextToken = takeWhile isSpace r
+        nextToken = takeWhile isNotSpecial r
 
+-- Building AST
+data LObject = 
+    LVar String |
+    LFunc String [LObject] |
+    LObjList [LObject]
+  deriving Show
+
+build :: [LToken] -> ([LObject], [LToken])
+build [] = ([], [])
+build d@(LClose:rs) = ([], rs)
+build d@(LOpen:rs)   
+    | length inside == 1 = ((head inside):same, up)
+    | otherwise          = ((LObjList inside):same, up)
+    where
+        (inside, rest) = build rs
+        (same, up) = build rest
+
+build d@((LNamed x):rs) = ((LVar x):same, rest)
+    where
+        (same, rest) = build rs
+
+build d@(LLambda:(LNamed x):LDot:rs) = ((LFunc x inside):same, up)
+    where
+        (inside, rest) = build rs
+        (same, up) = build rest
+
+build d = error "Error while parsing"
